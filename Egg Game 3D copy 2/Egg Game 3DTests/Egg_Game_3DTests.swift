b5a6ee5d8dae4ee4db36d3c4ead6606bca0b821b
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 22:16:09 -0400

    Add Egg Game 3D copy 2/Egg Game 3DTests/Egg_Game_3DTests.swift

---
+++ Egg Game 3D copy 2/Egg Game 3DTests/Egg_Game_3DTests.swift
@@ -0,0 +1,17 @@
+//
+//  Egg_Game_3DTests.swift
+//  Egg Game 3DTests
+//
+//  Created by Elliot Williams on 2025-06-22.
+//
+
+import Testing
+@testable import Egg_Game_3D
+
+struct Egg_Game_3DTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
