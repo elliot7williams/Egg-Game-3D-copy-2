933e4d24c0b26e7c61d2760178f4d6fabcde9f40
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 22:16:02 -0400

    Add Egg Game 3D copy 2/Egg Game 3D/Egg_Game_3DApp.swift

---
+++ Egg Game 3D copy 2/Egg Game 3D/Egg_Game_3DApp.swift
@@ -0,0 +1,17 @@
+//
+//  Egg_Game_3DApp.swift
+//  Egg Game 3D
+//
+//  Created by Elliot Williams on 2025-06-22.
+//
+
+import SwiftUI
+
+@main
+struct Egg_Game_3DApp: App {
+    var body: some Scene {
+        WindowGroup {
+            EggCatcherGame()
+        }
+    }
+}
