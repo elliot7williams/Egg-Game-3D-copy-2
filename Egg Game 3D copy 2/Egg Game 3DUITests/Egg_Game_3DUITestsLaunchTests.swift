3d179fa5a341bc56d0d510361f03ec8736f4f49f
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 22:16:00 -0400

    Add Egg Game 3D copy 2/Egg Game 3DUITests/Egg_Game_3DUITestsLaunchTests.swift

---
+++ Egg Game 3D copy 2/Egg Game 3DUITests/Egg_Game_3DUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Egg_Game_3DUITestsLaunchTests.swift
+//  Egg Game 3DUITests
+//
+//  Created by Elliot Williams on 2025-06-22.
+//
+
+import XCTest
+
+final class Egg_Game_3DUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
